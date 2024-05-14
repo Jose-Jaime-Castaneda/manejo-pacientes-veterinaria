94f912a34298e114905bb8b6f4e1445628b991f6
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-14 15:47:12 -0600

    Manage content rendering

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -3,14 +3,25 @@ import { usePatientStore } from "../store/store"
 export default function PatientsList() {
     const { patients } = usePatientStore()
     return (
-        <div>
-            {patients.map(patient => (
-                <div key={patient.id}>
-                    <p>
-                        {patient.name}
+        <div className=" md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+            {patients.length ? (
+                <>
+                    {patients.map((patient) => (
+                        <div key={patient.id}>
+                            <p>
+                                {patient.name}
+                            </p>
+                        </div>
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h2 className=" font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className=" text-xl mt-5 mb-10 text-center">Comienza agregando pacientes {''}
+                        <span className=" text-indigo-600 font-bold">y aparecerán en este lugar</span>
                     </p>
-                </div>
-            ))}
+                </>
+            )}
         </div>
     )
 }
