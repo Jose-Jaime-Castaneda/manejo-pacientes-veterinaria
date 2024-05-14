914806440ffece2b1e41aa43cab0acec1e37f519
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-14 11:23:24 -0600

    Styling the error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,6 +1,6 @@
 export default function Error({ children }: { children: React.ReactNode }) {
     return (
-        <p className=" text-center my-4 bg-red-600 text-white font-bold p-3 uppercase text-sm">
+        <p className=" text-center bg-red-600 text-white font-bold p-3 uppercase text-sm rounded-b-full">
             {children}
         </p>
     );
